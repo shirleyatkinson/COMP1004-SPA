2eb05483ffcc1f8a16fdea9d935bb1be11af996a
s1atkinson <s1atkinson@DESK-B-P0-11>
2021-11-19 13:33:44 +0000

    Added treasure game

--- wwwroot/js/spa.js
+++ wwwroot/js/spa.js
@@ -3,4 +3,20 @@
     $('#map').click(function () {
         $('#page-content-wrapper').load('map.html');
     })
+
+    $('#treasure').click(function () {
+        $('#page-content-wrapper').load('treasure.html');
+    })
+
+    $('#data').click(function () {
+        $('#page-content-wrapper').load('data.html');
+    })
+
+    $('#SubmitGuess').click(function () {
+        window.alert('I am here');
+
+        var player = $('#Player_Name').val();
+        var guess = $('#Guess').val();
+        window.alert("Well done " + player + ".  You have guessed " + guess);
+    })
 });
